e3d420527bae5de53e28cd7155a67bc99e7122dd
kobake <kobake@users.sourceforge.net>
2016-12-06 17:44:23 +0900

    Add project files.

---
+++ AspNetGitHubAuth/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetGitHubAuth.MvcApplication" Language="C#" %>
---
+++ AspNetGitHubAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetGitHubAuth.Startup))]
+namespace AspNetGitHubAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
